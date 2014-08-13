e5b9931194c133d29bcfab65062005c7a531a806
David Orn Johannsson <davidoj@puppet.local>
2014-08-13 04:05:00 -0700

    added apache and base profiles

---
+++ manifests/apache.pp
@@ -0,0 +1,5 @@
+# == Class: profile::apache## Apache profile## === Parameters## None## === Variables## None## === Examples##  include profile::apache## === Authors## Rob Nelson <rnelson0@gmail.com>## === Copyright## Copyright 2014 Rob Nelson
+#
+class profile::apache {
+  class {'::apache': }
+}
